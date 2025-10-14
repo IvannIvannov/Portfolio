eb4769f6fe3e8105f1971beef83351c3e3425252
IvannIvannov <151462104+IvannIvannov@users.noreply.github.com>
2025-10-14 19:06:54 +0300

    add new animation in home page

---
+++ public/src/aboutAnimation.js
@@ -0,0 +1,17 @@
+// Scroll reveal
+const reveals = document.querySelectorAll('.reveal');
+
+function revealOnScroll() {
+    for (let i = 0; i < reveals.length; i++) {
+        const windowHeight = window.innerHeight;
+        const elementTop = reveals[i].getBoundingClientRect().top;
+        const revealPoint = 150;
+
+        if (elementTop < windowHeight - revealPoint) {
+            reveals[i].classList.add('visible');
+        }
+    }
+}
+
+window.addEventListener('scroll', revealOnScroll);
+window.addEventListener('load', revealOnScroll);
